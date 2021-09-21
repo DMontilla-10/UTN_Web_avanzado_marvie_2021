5066dc402d309a7846a2d04ce67e675f51f8b3ee
Daniel Montilla <montilla.daniel@reanswer.com.ar>
2021-09-21 20:09:43 -0300

    Clase04 - Prmera parte

--- Clase04/index.js
+++ Clase04/index.js
@@ -1,6 +1,19 @@
 const express = require("express");
 require("dotenv").config();
 const userRoutes = require("./routes/users");
+const mongoose = require("mongoose");
+
+mongoose.connect(
+  process.env.MONGO_URI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+mongoose.connection.on("connected", () => {
+  console.log("Mongo está conectado exitosamente !!!");
+});
 
 const app = express();
 const port = process.env.PORT;
@@ -9,7 +22,7 @@ const port = process.env.PORT;
 app.use(express.json());
 
 // Routes
-app.use('/api/users', userRoutes)
+app.use("/api/users", userRoutes);
 
 app.listen(port, () => {
   console.log(`Servidor corriendo en puerto ${port}...`);
